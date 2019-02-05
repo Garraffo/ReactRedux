522dae92879cc08cf8ed539e5e1df78ca8619515
Ezequiel Garraffo <egarraffo@gmail.com>
2019-02-04 23:40:54 -0300

    provider

--- React-Redux-Boilerplate-master/dev/js/reducers/reducer-users.js
+++ React-Redux-Boilerplate-master/dev/js/reducers/reducer-users.js
@@ -1,32 +1,27 @@
-/*
- * The users reducer will always return an array of users no matter what
- * You need to return something, so if there are no users then just return an empty array
- * */
-
-export default function () {
-    return [
+export default function(){
+    return[
         {
             id: 1,
-            first: "Bucky",
-            last: "Roberts",
-            age: 71,
-            description: "Bucky is a React developer and YouTuber",
+            nombre: "Jorgelino",
+            apellido: "Salas",
+            age: 69,
+            descripcion: "Abogado",
             thumbnail: "http://i.imgur.com/7yUvePI.jpg"
         },
         {
             id: 2,
-            first: "Joby",
-            last: "Wasilenko",
-            age: 27,
-            description: "Joby loves the Packers, cheese, and turtles.",
+            nombre: "Ernesto",
+            apellido: "Bavastro",
+            age: 57,
+            descripcion: "Profesor",
             thumbnail: "http://i.imgur.com/52xRlm8.png"
         },
         {
             id: 3,
-            first: "Madison",
-            last: "Williams",
-            age: 24,
-            description: "Madi likes her dog but it is really annoying.",
+            nombre: "Miguel",
+            apellido: "Ioppolo",
+            age: 43,
+            descripcion: "Desarrollador",
             thumbnail: "http://i.imgur.com/4EMtxHB.png"
         }
     ]
